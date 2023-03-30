f564f8cd025e30913ceb6ef9a5789b68ac7a2bb8
Emre Ergül <emredevsalot@gmail.com>
2023-03-30 17:15:41 +0300

    added basic design system (colors and fonts)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <h1>H1 tag</h1>
+      <p>hello</p>
+    </div>
+  );
 }
 
 export default App;
