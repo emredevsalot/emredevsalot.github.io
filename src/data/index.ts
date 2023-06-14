981e01f02c68668423d9b319d91456b92df50040
Emre Ergül <emredevsalot@gmail.com>
2023-06-14 19:11:18 +0300

    add images to projects

--- src/data/index.ts
+++ src/data/index.ts
@@ -29,8 +29,8 @@ export const projects: Array<ProjectType> = [
     title: "Scene Struggle",
     description:
       "Scene Struggle allows users to play mini-games based on their favorite YouTube channels. Allowed me to explore the YouTube API and integrate it into a web application.",
-    backgroundImageUrl: "/project-example.png",
-    focusImageUrl: "/project-example-top.png",
+    backgroundImageUrl: "/scene-struggle.png",
+    focusImageUrl: "/scene-struggle-top.png",
     liveSiteUrl: "https://emredevsalot.github.io/scene-struggle/",
     githubUrl: "https://github.com/emredevsalot/scene-struggle",
     usedTech: [
@@ -45,8 +45,8 @@ export const projects: Array<ProjectType> = [
     title: "Gigachad Maker",
     description:
       "Gigachad Maker allows you to create a 'Gigachad Meme' GIF by leveraging the power of the Remotion framework for React.",
-    backgroundImageUrl: "/project-example.png",
-    focusImageUrl: "/project-example-top.png",
+    backgroundImageUrl: "/gigachad-maker.png",
+    focusImageUrl: "/gigachad-maker-top.png",
     liveSiteUrl: "",
     githubUrl: "https://github.com/emredevsalot/gigachad-maker",
     usedTech: ["React", "Typescript", "Tailwind", "Remotion"],
