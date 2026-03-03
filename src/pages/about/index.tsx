29a4fce2b8f3894026bbbdc9f24dd527da884684
emredevsalot <72770244+emredevsalot@users.noreply.github.com>
2026-03-03 15:36:39 +0300

    test commit

--- src/pages/about/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const About = (props: Props) => {
-  return <section>About</section>;
-};
-
-export default About;
--- src/styles/index.ts
+++
@@ -1,6 +0,0 @@
-const styles = {
-  sectionClass:
-    "flex flex-col items-center gap-8 md:flex-row py-[clamp(3rem,5vw,5rem)] ",
-};
-
-export default styles;
